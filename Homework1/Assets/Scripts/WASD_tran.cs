54c999c3e09a5dd8b97d5b3910cfee56340bcb2f
bunibell <158766692+bunibell@users.noreply.github.com>
2024-09-12 17:09:40 -0400

    added tran movement

--- Homework1/Assets/Scripts/WASD_tran.cs
+++ Homework1/Assets/Scripts/WASD_tran.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WASD_tran : MonoBehaviour
 {
+    //Variables 
+    public float speed = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,29 @@ public class WASD_tran : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Find the position of the object every frame and make a vector 
+        Vector2 pos = transform.position;
+
+        //If I press right arrow move the position to the right 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        //If I press left arrow move the position to the left 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        //If I press up arrow move the position up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        //If I press the down arrow move the position down 
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
     }
 }
