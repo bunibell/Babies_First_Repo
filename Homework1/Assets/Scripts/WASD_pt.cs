bb57629e8b83eac64a5de02a2547bbcd1549805a
bunibell <158766692+bunibell@users.noreply.github.com>
2024-09-12 17:36:34 -0400

    added movement with ChatGPT

---
+++ Homework1/Assets/Scripts/WASD_pt.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_pt : MonoBehaviour
+{
+    //Variables 
+    public float movespeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the rigid body on this script
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get input from WASD keys 
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    //Fixed update is called on a fixed value of frames 
+    private void FixedUpdate()
+    {
+        //Move the player
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}
